d1e9ee4f53bfeaae73068d4e61387c0fc67662b0
drerek <adrerek@gmail.com>
2018-07-02 14:41:37 +0300

    Initial commit

---
+++ src/main/java/com/privatecompany/app/MoneyTool/MoneyToolApplication.java
@@ -0,0 +1,15 @@
+package com.privatecompany.app.MoneyTool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+public class MoneyToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyToolApplication.class, args);
+	}
+}
