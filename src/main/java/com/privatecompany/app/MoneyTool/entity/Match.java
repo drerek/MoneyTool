d9bb91e72006dddb4df50b5e79c3deb8f1589ce6
drerek <adrerek@gmail.com>
2018-10-13 10:34:34 +0300

    regex

--- src/main/java/com/privatecompany/app/MoneyTool/entity/Match.java
+++ src/main/java/com/privatecompany/app/MoneyTool/entity/Match.java
@@ -5,6 +5,9 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.apache.commons.text.similarity.JaroWinklerDistance;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -13,6 +16,7 @@ public class Match {
     private Command awayCommand;
     private String startTime;
     private final JaroWinklerDistance distance = new JaroWinklerDistance();
+    private static final Pattern pattern = Pattern.compile("^.+\\sU\\d{2}$");
 
     @Override
     public String toString() {
@@ -22,18 +26,36 @@ public class Match {
                 " at " + startTime;
     }
 
-    public boolean compareMatchName(Match secondMatch){
+    public boolean compareMatchName(Match secondMatch) {
 
-        if (distance.apply(this.getHomeCommand().getName(),secondMatch.getHomeCommand().getName())>0.8
-                && distance.apply(this.getAwayCommand().getName(),secondMatch.getAwayCommand().getName())>0.6)
+        String thisHomeCommand = this.getHomeCommand().getName();
+        String thisAwayCommand = this.getAwayCommand().getName();
+        String secondHomeCommand = secondMatch.getHomeCommand().getName();
+        String secondAwayCommand = secondMatch.getAwayCommand().getName();
+
+        Matcher thisMatcherHome = pattern.matcher(thisHomeCommand);
+        Matcher thisMatcherAway = pattern.matcher(thisAwayCommand);
+        Matcher secondMatcherHome = pattern.matcher(secondHomeCommand);
+        Matcher secondMatcherAway = pattern.matcher(secondAwayCommand);
+
+        if (thisMatcherHome.find() && thisMatcherAway.find() && secondMatcherHome.find() && secondMatcherAway.find()) {
+            thisHomeCommand = thisHomeCommand.substring(0, thisHomeCommand.indexOf(thisMatcherHome.start()));
+            thisAwayCommand = thisAwayCommand.substring(0, thisAwayCommand.indexOf(thisMatcherAway.start()));
+            secondHomeCommand = secondHomeCommand.substring(0, secondHomeCommand.indexOf(secondMatcherHome.start()));
+            secondAwayCommand = secondAwayCommand.substring(0, secondAwayCommand.indexOf(secondMatcherAway.start()));
+
+        }
+
+        if (distance.apply(thisHomeCommand, secondHomeCommand) > 0.8
+                && distance.apply(thisAwayCommand, secondAwayCommand) > 0.6)
             return true;
-        if (distance.apply(this.getHomeCommand().getName(), secondMatch.getAwayCommand().getName())>0.8
-                && distance.apply(this.getAwayCommand().getName(), secondMatch.getHomeCommand().getName())>0.6)
+        if (distance.apply(thisHomeCommand, secondAwayCommand) > 0.8
+                && distance.apply(thisAwayCommand, secondHomeCommand) > 0.6)
             return true;
         return false;
     }
 
-    public boolean compareMatchTime(Match secondMatch){
+    public boolean compareMatchTime(Match secondMatch) {
         return this.getStartTime().equals(secondMatch.getStartTime());
     }
 }
