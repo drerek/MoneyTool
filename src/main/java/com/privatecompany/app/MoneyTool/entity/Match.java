5e787d29659c5ce69075776f11ba750d67d68dc3
drerek <adrerek@gmail.com>
2018-10-16 18:56:04 +0300

    fix time pattern + fix U leagues

--- src/main/java/com/privatecompany/app/MoneyTool/entity/Match.java
+++ src/main/java/com/privatecompany/app/MoneyTool/entity/Match.java
@@ -5,12 +5,10 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.apache.commons.text.similarity.JaroWinklerDistance;
 
-import java.time.LocalDate;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
-import static java.time.format.DateTimeFormatter.ISO_LOCAL_TIME;
 
 @Data
 @NoArgsConstructor
@@ -42,25 +40,31 @@ public class Match {
         Matcher secondMatcherHome = pattern.matcher(secondHomeCommand);
         Matcher secondMatcherAway = pattern.matcher(secondAwayCommand);
 
+        String thisLeague = "";
+        String secondLeague = "";
+
         if (thisMatcherHome.find() && thisMatcherAway.find() && secondMatcherHome.find() && secondMatcherAway.find()) {
             thisHomeCommand = thisHomeCommand.substring(0, thisMatcherHome.start());
             thisAwayCommand = thisAwayCommand.substring(0, thisMatcherAway.start());
+            thisLeague = this.getHomeCommand().getName().substring(thisMatcherHome.start()+1, this.getHomeCommand().getName().length());
             secondHomeCommand = secondHomeCommand.substring(0, secondMatcherHome.start());
             secondAwayCommand = secondAwayCommand.substring(0, secondMatcherAway.start());
+            secondLeague = secondMatch.getHomeCommand().getName().substring(secondMatcherHome.start()+1, secondMatch.getHomeCommand().getName().length());
         }
-
-        if (distance.apply(thisHomeCommand, secondHomeCommand) > 0.8
-                && distance.apply(thisAwayCommand, secondAwayCommand) > 0.6)
-            return true;
-        if (distance.apply(thisHomeCommand, secondAwayCommand) > 0.8
-                && distance.apply(thisAwayCommand, secondHomeCommand) > 0.6)
-            return true;
+        if (thisLeague.equals(secondLeague)) {
+            if (distance.apply(thisHomeCommand, secondHomeCommand) > 0.8
+                    && distance.apply(thisAwayCommand, secondAwayCommand) > 0.6)
+                return true;
+            if (distance.apply(thisHomeCommand, secondAwayCommand) > 0.8
+                    && distance.apply(thisAwayCommand, secondHomeCommand) > 0.6)
+                return true;
+        }
         return false;
     }
 
     public boolean compareMatchTime(Match secondMatch) {
-        LocalDate firstMatchTime = LocalDate.parse(this.getStartTime(), formatter);
-        LocalDate secondMatchTime = LocalDate.parse(secondMatch.getStartTime(), formatter);
+        LocalTime firstMatchTime = LocalTime.parse(this.getStartTime(), formatter);
+        LocalTime secondMatchTime = LocalTime.parse(secondMatch.getStartTime(), formatter);
         return firstMatchTime.isAfter(secondMatchTime);
     }
 }
